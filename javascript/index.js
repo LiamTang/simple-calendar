5704711df50e00528419c25d38c24bcd60e0053c
Liam Tang <tangwangjun123@gmail.com>
2020-08-02 00:55:33 +1000

    add basic feature on the calendar, allow users to check previous and next month

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,22 @@
 "use strict";
+let date = new Date();
 
 window.onload = function () {
   renderDate();
+
+  document.getElementById("prevMonth").onclick = function () {
+    date.setMonth(date.getMonth() - 1);
+    renderDate();
+  };
+  document.getElementById("nextMonth").onclick = function () {
+    date.setMonth(date.getMonth() + 1);
+    renderDate();
+  };
 };
 function renderDate() {
-  let date = new Date();
   let currentYear = date.getFullYear();
   let currentMonth = date.getMonth();
+  let currentDay = date.getDate();
   let months = [
     "January",
     "February",
@@ -21,11 +31,23 @@ function renderDate() {
     "November",
     "December",
   ];
-  let dayText = "";
-  for (let i = 0; i < 30; i++) {
-    dayText += "<li>" + i + "</li>";
-  }
 
+  //get the value => each month's first day is on which week
+  let week = new Date(currentYear, currentMonth, 1).getDay();
+  // get how many days every month = the last day every month
+  let days = new Date(currentYear, currentMonth + 1, -1).getDate() + 1;
+
+  let dayText = "";
+  for (let i = 0; i < week; i++) {
+    dayText += "<li></li>";
+  }
+  for (let i = 1; i <= days; i++) {
+    if (i === currentDay) {
+      dayText += "<li class=active>" + i + "</li>";
+    } else {
+      dayText += "<li class=hover>" + i + "</li>";
+    }
+  }
   document.getElementById("year").innerHTML = currentYear;
   document.getElementById("month").innerHTML = months[currentMonth];
   document.getElementById("day").innerHTML = dayText;
