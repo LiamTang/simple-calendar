681d86d3235163603f5c8f7af78d5b089e62522c
Liam Tang <tangwangjun123@gmail.com>
2020-08-04 23:22:47 +1000

    add new feature: click month title to show all months, click specific month show days

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,20 @@
 "use strict";
 let date = new Date();
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const cMonth = date.getMonth(); //the value of current month, never change
 
 window.onload = function () {
   renderDate();
@@ -12,43 +27,44 @@ window.onload = function () {
     date.setMonth(date.getMonth() + 1);
     renderDate();
   };
+
+  document.getElementById("month").onclick = function () {
+    renderMonth();
+  };
 };
+
 function renderDate() {
   let currentYear = date.getFullYear();
   let currentMonth = date.getMonth();
   let currentDay = date.getDate();
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
 
   //get the value => each month's first day is on which week
   let week = new Date(currentYear, currentMonth, 1).getDay();
   // get how many days every month = the last day every month
   let days = new Date(currentYear, currentMonth + 1, -1).getDate() + 1;
 
+  document.getElementById("year").innerHTML = currentYear;
+  document.getElementById("month").innerHTML = months[currentMonth];
+
   let dayText = "";
   for (let i = 0; i < week; i++) {
     dayText += "<li></li>";
   }
   for (let i = 1; i <= days; i++) {
-    if (i === currentDay) {
-      dayText += "<li class=active>" + i + "</li>";
+    if (i === currentDay && currentMonth === cMonth) {
+      dayText += "<li id=active class=active-style>" + i + "</li>";
     } else {
-      dayText += "<li class=hover>" + i + "</li>";
+      dayText += "<li>" + i + "</li>";
     }
   }
-  document.getElementById("year").innerHTML = currentYear;
-  document.getElementById("month").innerHTML = months[currentMonth];
+
   document.getElementById("day").innerHTML = dayText;
+}
+
+function renderMonth() {
+  let monthText = "";
+  for (let i = 0; i < months.length; i++) {
+    monthText += "<li>" + months[i] + "</li>";
+  }
+  document.querySelector("show-month").innerHTML = monthText;
 }
