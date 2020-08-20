9a6c4b4edc78b7e0d255b5a240150ae29f1ad954
Liam Tang <tangwangjun123@gmail.com>
2020-08-20 21:01:03 +1000

    add new features to calender - show month, show days depends on user selection

--- javascript/index.js
+++ javascript/index.js
@@ -1,39 +1,53 @@
-"use strict";
+'use strict';
 let date = new Date();
 let months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
 ];
 const cMonth = date.getMonth(); //the value of current month, never change
+
+//get 20 years value
+let years = [];
+for (let i = 2010; i < 2030; i++) {
+  years.push(i);
+}
 
 window.onload = function () {
   renderDate();
 
-  document.getElementById("prevMonth").onclick = function () {
+  document.getElementById('prevMonth').onclick = function () {
     date.setMonth(date.getMonth() - 1);
     renderDate();
   };
-  document.getElementById("nextMonth").onclick = function () {
+  document.getElementById('nextMonth').onclick = function () {
     date.setMonth(date.getMonth() + 1);
     renderDate();
   };
 
-  document.getElementById("month").onclick = function () {
+  document.getElementById('curMonth').onclick = function () {
+    document.querySelector('.bottom').classList.toggle('hide');
+    document.querySelector('.bottom-show-month').classList.toggle('active');
     renderMonth();
-    document.querySelector(".show-week").classList.toggle("hide");
-    document.querySelector(".show-month").classList.toggle("active");
-    const monthShow = document.querySelector(".show-month");
-    monthShow.onclick = function () {};
+  };
+
+  document.querySelector('.bottom-show-month').onclick = function (e) {
+    document.querySelector('.bottom-show-month').classList.remove('active');
+    document.querySelector('.bottom').classList.remove('hide');
+    const monthIndex = months.findIndex(
+      (value) => value === e.target.innerHTML
+    );
+    renderDateBySelection(monthIndex);
+    console.log(years);
   };
 };
 
@@ -47,32 +61,54 @@ function renderDate() {
   // get how many days every month = the last day every month
   let days = new Date(currentYear, currentMonth + 1, -1).getDate() + 1;
 
-  document.getElementById("year").innerHTML = currentYear;
-  document.getElementById("month").innerHTML = months[currentMonth];
+  document.getElementById('year').innerHTML = currentYear;
+  document.getElementById('curMonth').innerHTML = months[currentMonth];
 
-  let dayText = "";
+  let dayText = '';
   for (let i = 0; i < week; i++) {
-    dayText += "<li></li>";
+    dayText += '<li></li>';
   }
   for (let i = 1; i <= days; i++) {
     if (i === currentDay && currentMonth === cMonth) {
-      dayText += "<li id=active class=active-style>" + i + "</li>";
+      dayText += '<li id=active class=active-style>' + i + '</li>';
     } else {
-      dayText += "<li>" + i + "</li>";
+      dayText += '<li>' + i + '</li>';
     }
   }
 
-  document.getElementById("day").innerHTML = dayText;
+  document.getElementById('day').innerHTML = dayText;
 }
 
 function renderMonth() {
-  let monthText = "";
+  let monthText = '';
   for (let i = 0; i < months.length; i++) {
-    if (months[i] === document.getElementById("month").innerHTML) {
-      monthText += "<li class=month-highlight>" + months[i] + "</li>";
+    if (months[i] === months[cMonth]) {
+      monthText += '<li class = month-highlight>' + months[i] + '</li>';
     } else {
-      monthText += "<li>" + months[i] + "</li>";
+      monthText += '<li>' + months[i] + '</li>';
     }
   }
-  document.querySelector(".show-month").innerHTML = monthText;
+  document.querySelector('.bottom-show-month').innerHTML = monthText;
+}
+
+function renderDateBySelection(month) {
+  let currentYear = date.getFullYear();
+  let currentDay = date.getDate();
+  let week = new Date(currentYear, month, 1).getDay();
+  let days = new Date(currentYear, month + 1, -1).getDate() + 1;
+
+  document.getElementById('curMonth').innerHTML = months[month];
+
+  let dayText = '';
+  for (let i = 0; i < week; i++) {
+    dayText += '<li></li>';
+  }
+  for (let i = 1; i <= days; i++) {
+    if (i === currentDay && month === cMonth) {
+      dayText += '<li id=active class=active-style>' + i + '</li>';
+    } else {
+      dayText += '<li>' + i + '</li>';
+    }
+  }
+  document.getElementById('day').innerHTML = dayText;
 }
